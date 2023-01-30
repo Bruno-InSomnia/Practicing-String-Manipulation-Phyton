304f9241e8887657087c37cb957b94adc03aa66c
Bruno <brox.fs@gmail.com>
2023-01-29 21:26:29 -0300

    Sanitizing and validating strings.

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-url = 'https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100'
 
+# URL de exemplo
+# url = 'https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100'
+url = " "
+
+# Sanitização e Validação da URL
+if url.strip() == "":
+    raise ValueError("A URL está vazia")
+
+# Separando base e parâmetros
 indice_interrogacao = url.find('?')
-
 url_base = url[:indice_interrogacao]
+url_parametros = url[indice_interrogacao + 1:]
 
-url_parametro = url[indice_interrogacao+1:]
-print(url_parametro)
-
+# Busca o valor de um parâmetro
+parametro_busca = 'quantidade'
+indice_do_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_do_parametro + len(parametro_busca) + 1
+indice_e_comercial = url_parametros.find('&', indice_valor)
+if indice_e_comercial == -1:
+    valor_busca = url_parametros[indice_valor:]
+else:
+    valor_busca = url_parametros[indice_valor:indice_e_comercial]
+print(valor_busca)
